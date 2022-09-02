f981cfd564104d379827401b63e3e15d6f85458c
shipatulhasan <shipatulhasan328@gmail.com>
2022-09-02 11:20:49 +0600

    adding

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const showAll = async() =>{
             const tabItem = document.createElement('kbd')
             
             tabItem.setAttribute('onclick','searchTab(event)')
-            tabItem.classList.add('kbd','mx-3','hover:cursor-pointer','bg-transparent')
+            tabItem.classList.add('kbd','mx-3','hover:cursor-pointer','bg-gray-100')
             tabItem.innerText = product.category
             menuContainer.appendChild(tabItem)
             
